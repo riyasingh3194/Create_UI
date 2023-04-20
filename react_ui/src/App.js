1fdf0106acb7265f0fdbca1e8bec3c36b7b699c6
riya singh <riyafriday5@gmail.com>
2023-04-20 06:55:12 +0530

    push in main branch

---
+++ react_ui/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+       
+      </header>
+      <h1 >
+        Benifts of using react...
+      </h1>
+      <p>
+        1.component based architecture
+      </p>
+      <p>
+       2. virual DOM for efficient updates
+      </p>
+      <p>
+        3.Rich ecosystem and community
+      </p>
+      <p>
+        4.Cross platform development</p>
+      <p>
+        5.Strong community support
+      </p>
+      <button>
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default App;
